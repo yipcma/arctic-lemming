c5c1d10a8039786d64337b1724d0309805945e32
Andrew Yip <yipcma@gmail.com>
2017-10-11 18:25:23 +0300

    add game-icons, use extends in yml

--- 04_save_pdf.rb
+++ 04_save_pdf.rb
@@ -1,14 +1,15 @@
 require 'squib'
 
 data = Squib.csv file: 'data.csv'
-
-Squib::Deck.new cards: data['name'].size, layout: 'economy.yml' do
+layouts = ['economy.yml', 'cards.yml']
+Squib::Deck.new cards: data['name'].size, layout: layouts do
   background color: 'white'
   rect layout: 'cut'
   rect layout: 'safe'
   text str: data['name'], layout: 'title'
   text str: data['power'], layout: 'description'
   text str: Time.now, layout: 'credits'
-  save_png
+  svg layout: data['faction']
+  save_sheet prefix: 'part2_', columns: 4
   save_pdf trim: 37.5 # 1/8-inch trim (300ppi/8) on 8.5x11 landscape
 end
