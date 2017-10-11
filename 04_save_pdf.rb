1e88fd455ef42e0bfd7cf2ab704e8a1b7225e20b
Andrew Yip <yipcma@gmail.com>
2017-10-11 18:32:56 +0300

    add illustrations and save_sheet with prefix

--- 04_save_pdf.rb
+++ 04_save_pdf.rb
@@ -10,6 +10,7 @@ Squib::Deck.new cards: data['name'].size, layout: layouts do
   text str: data['power'], layout: 'description'
   text str: Time.now, layout: 'credits'
   svg layout: data['faction']
+  svg file: data['illustration'], layout: 'illustration'
   save_sheet prefix: 'part2_', columns: 4
   save_pdf trim: 37.5 # 1/8-inch trim (300ppi/8) on 8.5x11 landscape
 end
